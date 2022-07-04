3d5eace6fef04ea40e6b9c3dc2502dac0fd68ba7
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-07-04 17:42:19 +0530

    Update Battery_Checker.c

--- Battery_Checker.c
+++ Battery_Checker.c
@@ -1,6 +1,20 @@
 #include "Battery_Checker.h"
 
-int isTemperatureOK(float temperature) {
+void updateBatteryStatus_ParameterRange(void){
+  //batteryCheck[TEMPERATURE].bms_Parameter.status = {FALSE, 0, 45};
+  batteryCheck[TEMPERATURE].minRange = 0;
+  batteryCheck[TEMPERATURE].maxRange = 45;
+  
+  //batteryCheck[SOC].bms_Parameter = FALSE;
+  batteryCheck[SOC].minRange = 20;
+  batteryCheck[SOC].maxRange = 80;
+  
+  //batteryCheck[CHARGE_RATE].bms_Parameter = FALSE;  
+  batteryCheck[CHARGE_RATE].minRange = 0.8;  
+  batteryCheck[CHARGE_RATE].maxRange = 1;
+}
+
+/*int isTemperatureOK(float temperature) {
   return checkOK(temperature, 0, 45);
 }
 
@@ -14,9 +28,14 @@ int isChargeRateOK(float chargeRate) {
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
  return isTemperatureOK(temperature) && isSOCOK(soc) && isChargeRateOK(chargeRate);
+}*/
+
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+   return checkOK(TEMPERATURE, temperature) && checkOK(SOC, soc) && checkOK(CHARGE_RATE, chargeRate);
 }
 
 int main() {
+  updateBatteryStatus_ParameterRange();
   assert(!batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
 }

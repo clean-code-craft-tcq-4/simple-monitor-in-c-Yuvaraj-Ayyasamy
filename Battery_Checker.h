b219c60f31dc06a0bd902229f71ba6fd91cc4277
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-07-04 18:07:18 +0530

    Update Battery_Checker.h

--- Battery_Checker.h
+++ Battery_Checker.h
@@ -4,10 +4,33 @@
 #define NOT_OK 0
 #define OK     1
 
-#define checkOK( currentValue, minRange, maxRange )                           \
+typedef enum 
+{
+  TEMPERATURE_RANGE = 0,
+  SOC_RANGE,
+  CHARGE_RATE,
+}batteryFactors_e;
+
+typedef struct
+{
+  int checkStatus;
+  float minRange;
+  float maxRange;
+}batteryFactorsRange;
+
+batteryFactorsRange batteryFactorStatus[3];
+
+#define checkOK( currentValue, batteryFactor )                 \
+({                                                                  \
+    if ( (currentValue < batteryFactorStatus[batteryFactor].minRange) && (currentValue > batteryFactorStatus[batteryFactor].maxRange)) {  \
+        batteryFactorStatus[batteryFactor].checkStatus = OK;                                           \
+    }                                                               \
+})
+
+/*#define checkOK( currentValue, batteryFactor)                           \
 ({                                                                            \
     ((currentValue < minRange) && (currentValue > maxRange)) ? OK : NOT_OK;   \
-})
+})*/
 
 /* #define checkOK( currentValue, minRange, maxRange )                 \
 ({                                                                  \

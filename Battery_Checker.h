6d5dcac4eadaa1d77d30bafe3c65e781a0ff1050
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-07-04 20:25:04 +0530

    Update Battery_Checker.h

--- Battery_Checker.h
+++ Battery_Checker.h
@@ -4,11 +4,11 @@
 #define NOT_OK 0
 #define OK     1
 
- #define checkOK( currentValue, minRange, maxRange )                 \
-({                                                                  \
-    int checkResult = NOT_OK;                                       \
+ #define checkOK( currentValue, minRange, maxRange )                  \
+({                                                                    \
+    int checkResult = NOT_OK;                                         \
     if ( (currentValue >= minRange) && (currentValue <= maxRange)) {  \
-        checkResult = OK;                                           \
-    }                                                               \
-    checkResult;                                                    \
+        checkResult = OK;                                             \
+    }                                                                 \
+    checkResult;                                                      \
 })

7ebaf2928607973e563505aac65c202ced0103d5
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-07-04 18:08:45 +0530

    Update Battery_UpdateFactorsRange.c

--- Battery_UpdateFactorsRange.c
+++ Battery_UpdateFactorsRange.c
@@ -1,15 +1,15 @@
 #include "Battery_Checker.h"
 
 void updateBatteryFactorsRange() {
-  battery_checkcase[TEMPERATURE].checkStatus = NOT_OK;
-  battery_checkcase[TEMPERATURE].minRange = 0;
-  battery_checkcase[TEMPERATURE].maxRange = 45;
+  batteryFactorStatus[TEMPERATURE].checkStatus = NOT_OK;
+  batteryFactorStatus[TEMPERATURE].minRange = 0;
+  batteryFactorStatus[TEMPERATURE].maxRange = 45;
   
-  battery_checkcase[SOC].checkStatus = NOT_OK;
-  battery_checkcase[SOC].minRange = 20;
-  battery_checkcase[SOC].maxRange = 80;
+  batteryFactorStatus[SOC].checkStatus = NOT_OK;
+  batteryFactorStatus[SOC].minRange = 20;
+  batteryFactorStatus[SOC].maxRange = 80;
   
-  battery_checkcase[CHARGE_RATE].checkStatus = NOT_OK;  
-  battery_checkcase[CHARGE_RATE].minRange = 0.8;  
-  battery_checkcase[CHARGE_RATE].maxRange = 1; 
+  batteryFactorStatus[CHARGE_RATE].checkStatus = NOT_OK;  
+  batteryFactorStatus[CHARGE_RATE].minRange = 0.8;  
+  batteryFactorStatus[CHARGE_RATE].maxRange = 1; 
 }

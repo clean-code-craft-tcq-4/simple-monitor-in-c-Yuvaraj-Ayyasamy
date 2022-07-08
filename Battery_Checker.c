9989375077eb16242474d79f16b6653e41120979
Yuvaraj-Ayyasamy <105203963+Yuvaraj-Ayyasamy@users.noreply.github.com>
2022-07-08 17:05:38 +0530

    Update Battery_Checker.c

--- Battery_Checker.c
+++ Battery_Checker.c
@@ -17,6 +17,18 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
 }
 
 int main() {
+  /* All Temp, SOC & Charge Rate values are inside range */
   assert(batteryIsOk(25, 70, 0.9));
+  
+  /* Only Temp value outside range */
+  assert(!batteryIsOk(50, 70, 0.9));
+  
+    /* Only SOC value outside range */
+  assert(!batteryIsOk(30, 85, 0.9));
+  
+    /* Only Charge Rate outside range */
+  assert(!batteryIsOk(40, 70, 0.7));
+  
+  /* All Temp, SOC & Charge Rate values are outside range */
   assert(!batteryIsOk(50, 85, 0));
 }
